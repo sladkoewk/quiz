c1bb364f6981683168da47cc5be002ab840d22a0
sladkoewk <sladkoewk.dev@gmail.com>
2019-09-07 21:01:36 +0300

    Add end

--- src/js/modules/questionType/end.js
+++ src/js/modules/questionType/end.js
@@ -1,5 +1,13 @@
-function End(question) {
+import buttonElement from '../elements/buttonElement';
 
+function end(question, index, container, respond) {
+  const button = buttonElement(question.textButton, false);
+
+  container.append(button);
+
+  button.addEventListener('click', () => {
+    respond({ answer: new Date(), next: question.next, index });
+  });
 }
 
-export default End;
+export default end;
